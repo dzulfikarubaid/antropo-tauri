bce1d4edfb53988b82a189e75443467814becbf1
dzulfikarubaid <dzulfikarubaid@gmail.com>
2023-11-23 20:26:31 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className='flex parent h-full flex-col items-center gap-4 p-10 bg-white text-teks text-center justify-center py-10'>
     <motion.img initial={{ opacity: 1, rotateY:40 }} animate={{ opacity: 1, rotateY:0, transition: { duration: 10, } }} className='w-[400px]' src="/babywhite.png" alt="" />
-    <motion.h1 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1,y:0,  transition: { duration: 2, } }} className=' text-[40px] font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>Welcome to Baby Antrophometry GUI</motion.h1>
+    <motion.h1 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1,y:0,  transition: { duration: 2, } }} className=' text-[40px] font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>Baby Antrophometry GUI</motion.h1>
     <motion.p className='text-subteks font-light mb-4'>Measure the baby body using camera integrated with image processing and deep learning</motion.p>
     <a href="/camera" className='self-center border-[1px] border-black rounded-2xl p-2 px-10 bg-gradient-to-br from-white/40 via-white/20 to-transparent'>Go to camera</a>
     </div>
--- src/Cam.tsx
+++ src/Cam.tsx
@@ -16,7 +16,7 @@ const Cam = () => {
     const capture1 = useCallback(() => {
         const imageSrc = webcamRef1.current.getScreenshot();
         setImgSrc1(imageSrc);
-     
+
     }, [webcamRef1]);
     const capture2 = useCallback(() => {
         const imageSrc = webcamRef2.current.getScreenshot();
@@ -41,51 +41,56 @@ const Cam = () => {
     return (
 
         <div className="flex flex-col gap-10  justify-center p-10">
-         <button onClick={() => history.back()}><IoArrowBack size={30}></IoArrowBack></button>
+            <button onClick={() => history.back()}><IoArrowBack size={30}></IoArrowBack></button>
             {
-                !zoomImage1 && !zoomImage2 && <>
-                   
-                    <div className='flex flex-row gap-10 justify-center items-center relative'>
+                !zoomImage1 && !zoomImage2 && <div className="flex flex-row gap-4 justify-center items-center">
+
+                    <div className='justify-center items-center relative h-full'>
                         <div className="flex flex-col gap-3">
                             <motion.h1 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0, transition: { duration: 2, } }} className=' text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>Camera 1</motion.h1>
 
-                            <Webcam className="rounded-3xl" ref={webcamRef1} audio={false} videoConstraints={{ deviceId: devices[0]?.deviceId }} />
-                            {/* <h1 className="text-center">{devices[0]?.label}</h1> */}
+                            <Webcam className="rounded-xl" ref={webcamRef1} audio={false} videoConstraints={{ deviceId: devices[0]?.deviceId }} />
+                            <h1 className="text-center">{devices[0]?.label}</h1>
 
 
                         </div>
-                        <button className="text-purple-700 bg-gradient-to-br from-indigo-600/60 to-white/40 border-[1px] border-black/20 rounded-2xl p-4 h-fit" onClick={capture1}><IoCameraOutline size={40}></IoCameraOutline></button>
-                        <button onClick={() => setZoomImage1(!zoomImage1)} className="absolute bottom-0 right-3">
-                            <img className="w-[100px] rounded-xl" src={imgSrc1 || ''} alt="" />
-                        </button>
+                        <div className="flex flex-col gap-20">
+                        </div>
 
                     </div>
-                    <div className="flex flex-row gap-10 justify-center items-center relative">
+                    <div className=" justify-center items-center relative">
                         <div className="flex flex-col gap-3">
                             <motion.h1 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0, transition: { duration: 2, } }} className=' text-xl font-semibold  text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>Camera 2</motion.h1>
-                            <Webcam className="rounded-3xl" ref={webcamRef2} audio={false} videoConstraints={{ deviceId: devices[1]?.deviceId }} /></div>
-                        <button className="text-purple-700 bg-gradient-to-br from-indigo-600/60 to-white/40 border-[1px] border-black/20 rounded-2xl p-4 h-fit" onClick={capture2}><IoCameraOutline size={40}></IoCameraOutline></button>
-                        <button onClick={() => setZoomImage2(!zoomImage2)} className="absolute bottom-0 right-3">
-                            <img className="w-[100px] rounded-xl" src={imgSrc2 || ''} alt="" />
-                        </button>
-                    </div></>
+                            <Webcam className="rounded-xl" ref={webcamRef2} audio={false} videoConstraints={{ deviceId: devices[1]?.deviceId }} />
+                            <h1 className="text-center">{devices[1]?.label}</h1>
+                        </div>
+
+
+                        <div className="flex flex-col gap-20">
+                        </div>
+                    </div></div>
             }
+            <button className="text-purple-700  border-[1px] border-black/20 rounded-2xl p-4 h-fit text-center flex items-center justify-center" onClick={capture1}><IoCameraOutline size={40}></IoCameraOutline></button>
+            <button onClick={() => setZoomImage1(!zoomImage1)} className="">
+                <img className="w-[100px] rounded-xl" src={imgSrc1 || ''} alt="" />
+            </button>
 
             {zoomImage1 &&
-                <div className="absolute top-0 left-0 w-full h-full bg-black/30">
+                <div className="absolute top-0 left-0 w-full h-fit bg-black">
                     <div className="w-full px-10 py-5 bg-dongker flex-row text-white">
                         <button onClick={() => setZoomImage1(!zoomImage1)}><IoArrowBack size={30}></IoArrowBack></button>
                     </div>
-                    <img className="w-full h-full " src={imgSrc1 || ''} alt="" />
-                </div>}
+                    <img className="w-full h-screen object-contain" src={imgSrc1 || ''} alt="" />
+                </div>
+            }
 
 
             {zoomImage2 &&
-                <div className="absolute top-0 left-0 w-full h-full bg-black/30">
+                <div className="absolute top-0 left-0 w-full h-fit bg-black">
                     <div className="w-full px-10 py-5 bg-dongker flex-row text-white">
                         <button onClick={() => setZoomImage2(!zoomImage2)}><IoArrowBack size={30}></IoArrowBack></button>
                     </div>
-                    <img className="w-full h-full" src={imgSrc2 || ''} alt="" />
+                    <img className="w-full h-screen object-contain" src={imgSrc2 || ''} alt="" />
                 </div>}
 
 
